c0906d09e6e75c3cf03906be0ca8349583652ee2
Hanno Schlichting <hanno@jarn.com>
2010-12-24 13:46:20 +0100

    Rename history to changes

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.0'
 
@@ -8,8 +7,7 @@ setup(name='collective.ATClamAV',
       description="Provides ClamAV antivirus integration for Archetypes based "
           "content types",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+                       open("CHANGES.txt").read(),
       classifiers=[
           'Environment :: Web Environment',
           'Framework :: Plone',
