7f075bbcc226b288f610b627d5af3af2e5c0fc9f
Hanno Schlichting <hanno@jarn.com>
2011-01-11 11:20:25 +0100

    Avoid duplicated code and merge adapters into a single schema module

---
+++ src/collective/ATClamAV/schema.py
@@ -0,0 +1,27 @@
+from archetypes.schemaextender.interfaces import ISchemaModifier
+from Products.ATContentTypes.interfaces import IATFile
+from Products.ATContentTypes.interfaces import IATImage
+from zope.component import adapts
+from zope.interface import implements
+
+
+class VirusFreeModifier(object):
+    """
+    """
+    implements(ISchemaModifier)
+
+    def __init__(self, context):
+        self.context = context
+
+    def fiddle(self, schema):
+        schema[self.fieldname].validators.appendRequired('isVirusFree')
+
+
+class VirusFreeATFileModifier(VirusFreeModifier):
+    adapts(IATFile)
+    fieldname = 'file'
+
+
+class VirusFreeATImageModifier(VirusFreeModifier):
+    adapts(IATImage)
+    fieldname = 'image'
