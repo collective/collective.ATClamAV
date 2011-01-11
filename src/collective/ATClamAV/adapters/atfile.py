40c470f4ffeeb37048e42188b00d056a0cc489b2
Hanno Schlichting <hanno@jarn.com>
2011-01-11 11:10:56 +0100

    Replace schema extender with a schema modifier. Extenders don't have a stable ordering, so we cannot ensure to be called after plone.app.blob's extenders.

--- src/collective/ATClamAV/adapters/atfile.py
+++ src/collective/ATClamAV/adapters/atfile.py
@@ -1,35 +1,17 @@
-from archetypes.schemaextender.interfaces import ISchemaExtender
-from plone.app.blob.subtypes.file import ExtensionBlobField
-from Products.Archetypes.atapi import FileWidget, AnnotationStorage
-from Products.ATContentTypes import ATCTMessageFactory as _
+from archetypes.schemaextender.interfaces import ISchemaModifier
 from Products.ATContentTypes.interface import IATFile
-from Products.validation import V_REQUIRED
 from zope.component import adapts
 from zope.interface import implements
 
 
-class VirusFreeATFileExtender(object):
+class VirusFreeATFileModifier(object):
     """
     """
     adapts(IATFile)
-    implements(ISchemaExtender)
-    fields = [
-        ExtensionBlobField('file',
-                  required=True,
-                  primary=True,
-                  searchable=True,
-                  languageIndependent=True,
-                  storage = AnnotationStorage(migrate=True),
-                  validators = (('isNonEmptyFile', V_REQUIRED),
-                                ('checkFileMaxSize', V_REQUIRED),
-                                ('isVirusFree', V_REQUIRED)),
-                  widget = FileWidget(description='',
-                                      label=_(u'label_file', default=u'File'),
-                                      show_content_type=False)),
-    ]
+    implements(ISchemaModifier)
 
     def __init__(self, context):
         self.context = context
 
-    def getFields(self):
-        return self.fields
+    def fiddle(self, schema):
+        schema['file'].validators.appendRequired('isVirusFree')
--- src/collective/ATClamAV/adapters/atimage.py
+++ src/collective/ATClamAV/adapters/atimage.py
@@ -1,46 +1,17 @@
-from archetypes.schemaextender.interfaces import ISchemaExtender
-from plone.app.blob.subtypes.image import ExtensionBlobField
-from Products.Archetypes.atapi import ImageWidget, AnnotationStorage
-from Products.ATContentTypes.configuration import zconf
+from archetypes.schemaextender.interfaces import ISchemaModifier
 from Products.ATContentTypes.interfaces.image import IATImage
-from Products.ATContentTypes import ATCTMessageFactory as _
-from Products.validation import V_REQUIRED
 from zope.component import adapts
 from zope.interface import implements
 
 
-class VirusFreeATImageExtender(object):
+class VirusFreeATImageModifier(object):
     """
     """
     adapts(IATImage)
-    implements(ISchemaExtender)
-
-    fields = [
-        ExtensionBlobField('image',
-            required = True,
-            primary = True,
-            accessor = 'getImage',
-            mutator = 'setImage',
-            sizes = None,
-            languageIndependent = True,
-            storage = AnnotationStorage(migrate=True),
-            swallowResizeExceptions = zconf.swallowImageResizeExceptions.enable,
-            pil_quality = zconf.pil_config.quality,
-            pil_resize_algo = zconf.pil_config.resize_algo,
-            original_size = None,
-            max_size = zconf.ATImage.max_image_dimension,
-            default_content_type = 'image/png',
-            allowable_content_types = ('image/gif', 'image/jpeg', 'image/png'),
-            validators = (('isVirusFree', V_REQUIRED),
-                          ('isNonEmptyFile', V_REQUIRED),
-                          ('checkImageMaxSize', V_REQUIRED)),
-            widget = ImageWidget(label = _(u'label_image', default=u'Image'),
-                                 description=_(u''),
-                                 show_content_type = False,)),
-    ]
+    implements(ISchemaModifier)
 
     def __init__(self, context):
         self.context = context
 
-    def getFields(self):
-        return self.fields
+    def fiddle(self, schema):
+        schema['image'].validators.appendRequired('isVirusFree')
